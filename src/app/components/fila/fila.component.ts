060837e34a87606cf7eb2fe3df0b889cc999c689
Micaela Saez <mica_saez@hotmail.com>
2019-03-30 00:02:27 -0300

    Initial Commit

--- src/app/components/fila/fila.component.ts
+++ src/app/components/fila/fila.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { iterateListLike } from '@angular/core/src/change_detection/change_detection_util';
 
 @Component({
-  selector: '[app-fila]',
+  selector: '[app-fila]', // cambia la forma en la que se lo llama desde el html, anotacion en el cuaderno
   templateUrl: './fila.component.html',
   styleUrls: ['./fila.component.css']
 })
